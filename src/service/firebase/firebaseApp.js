414ead5306d4b930008be866895672ebabf82474
chaechae <dokailove6326@gmail.com>
2021-10-03 18:40:04 +0900

    firebase init & .env setting

---
+++ src/service/firebase/firebaseApp.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_APIKEY,
+  authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASEURL,
+  projectId: REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: REACT_APP_FIREBASE_STORAGEBUKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: REACT_APP_FIREBASE_APPID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;

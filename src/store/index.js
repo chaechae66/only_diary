8e5f6d05bba41045abe014dfbb003df07a1fad82
chaechae <dokailove6326@gmail.com>
2022-09-05 17:57:53 +0900

    yarn add redux-persist : rerandering error

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './userSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './userSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+const reducers = combineReducers({
+    user: userReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer: {
-        user : userReducer,
-    },
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }),

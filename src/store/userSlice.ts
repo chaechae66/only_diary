82250ebae607ff90054ea21f355ea8b7ac470936
chaechae <dokailove6326@gmail.com>
2022-10-31 20:37:42 +0900

    Chore: 리덕스 User 타입 수정

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,30 +1,30 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { CurrentUser, User } from '../types/types';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CurrentUser, User } from "../types/types";
 
-const initialState = {
-    currentUser : null,
-    isLoading : false,
-} as User
+const initialState: User = {
+  currentUser: null,
+  isLoading: false,
+};
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        user_login: (state : User, action : PayloadAction<CurrentUser>) => {
-        state.currentUser = action.payload;
-        state.isLoading = true;
+  name: "user",
+  initialState,
+  reducers: {
+    user_login: (state: User, action: PayloadAction<CurrentUser>) => {
+      state.currentUser = action.payload;
+      state.isLoading = true;
     },
-        user_logout: (state : User) => {
-        state.currentUser = null;
-        state.isLoading = true;
+    user_logout: (state: User) => {
+      state.currentUser = null;
+      state.isLoading = true;
     },
-        update_photo: (state : User ,action : PayloadAction<string>) => {
-        state.currentUser.photoURL = action.payload;
-        state.isLoading = true;
-        }
+    update_photo: (state: User, action: PayloadAction<string>) => {
+      state.currentUser.photoURL = action.payload;
+      state.isLoading = true;
     },
-})
+  },
+});
 
-export const { user_login, user_logout, update_photo } = userSlice.actions
+export const { user_login, user_logout, update_photo } = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
